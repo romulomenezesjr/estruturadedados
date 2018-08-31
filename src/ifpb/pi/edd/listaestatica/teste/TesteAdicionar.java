c77b07250068ccb7b9193a163b8acfc77ede1182
Romulo <romulojnr@gmail.com>
2018-08-31 10:13:47 -0300

    Mudando nome do projeto

--- src/ifpb/pi/edd/listaestatica/teste/TesteAdicionar.java
+++ src/ifpb/pi/edd/listaestatica/teste/TesteAdicionar.java
@@ -1,14 +1,13 @@
 package ifpb.pi.edd.listaestatica.teste;
 
 import ifpb.pi.edd.lista.Lista;
-import ifpb.pi.edd.listaencadeada.ListaEncadeada;
 import ifpb.pi.edd.listaestatica.ListaEstatica;
 
 public class TesteAdicionar {
 	public static void main(String[] args) {
 
 	
-		ListaEstatica lista = new ListaEstatica();
+		Lista lista = new ListaEstatica();
 		lista.inicializa();
 		lista.adicionar("Rômulo");
 		lista.adicionar("José");
